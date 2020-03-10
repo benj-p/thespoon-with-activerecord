8c9a4363e1896cbb468764993bec046f7ff7e896
Ben Pham <benjamin.pham@gmail.com>
2020-03-10 09:47:11 +0800

    Added routes and restaurant controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :restaurants
+
+  # CRUD
+
+  # Create
+
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+
+  # # Read
+
+  # get 'restaurants', to: 'restaurants#index'
+  # get 'restaurants/:id', to: 'restaurants#show'
+
+  # # Update
+
+  # get 'restaurants/:id/edit', to: 'restaurants#edit'
+  # patch 'restaurants/:id', to: 'restaurants#update'
+
+  # # Delete
+
+  # delete 'restaurants/:id', to: 'restaurants#destroy'
+
 end
