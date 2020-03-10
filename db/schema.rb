14cc57f37d2a5abc22fbe32473c2b31b3e957976
Ben Pham <benjamin.pham@gmail.com>
2020-03-10 09:20:45 +0800

    Added address to restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_010732) do
+ActiveRecord::Schema.define(version: 2020_03_10_011914) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
 end
